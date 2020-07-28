fc0220b05cad3dfa912f002dce7a84cf907bfcd8
lakto69 <leite.am@gmail.com>
2020-07-28 15:24:08 -0300

    Cap. 4

---
+++ ExploringLayouts/app/src/main/java/com/aml1969/exploringlayouts/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.aml1969.exploringlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main) // Apagando a linha original
+        setContentView(R.layout.main_menu)
+
+    }
+
+    fun loadConstraintLayout(v: View){
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadTableLayout(v: View){
+        setContentView(R.layout.my_table_layout)
+    }
+
+    fun loadMenuLayout(v: View){
+        setContentView(R.layout.main_menu)
+    }
+
+
+}
